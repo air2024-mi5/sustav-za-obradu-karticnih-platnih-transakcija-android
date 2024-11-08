3f838d027f97f214f55c4d7060ed22c1d806a301
bkadezabek <bkadezabe20@foi.hr>
2024-11-08 11:26:16 +0100

    Ažurirani su ComposableTestView
    Radi dodantog testiranja navigacije između njih

--- app/src/main/java/foi/air/szokpt/views/test_views/ComposableTestViews.kt
+++ app/src/main/java/foi/air/szokpt/views/test_views/ComposableTestViews.kt
@@ -1,21 +1,41 @@
 package foi.air.szokpt.views.test_views
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Button(onClick = { navController.navigate("details") }) {
-        Text("Details")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column {
+            Button(onClick = { navController.navigate("details") }) {
+                Text("Details")
+            }
+            Text(text = "Home")
+        }
     }
 }
 
 @Composable
 fun DetailsScreen(navController: NavController) {
-    Text("Details Screen")
-    Button(onClick = { navController.popBackStack() }) {
-        Text("Back")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column {
+            Text("Details Screen")
+            Button(onClick = { navController.popBackStack() }) {
+                Text("Back")
+            }
+        }
     }
 }
