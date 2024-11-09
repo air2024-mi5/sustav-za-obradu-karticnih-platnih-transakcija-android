abc85da280acc58e2fecc77b7197b0d95bc460c1
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2024-11-09 15:51:33 +0100

    MI5-25 Dodana tekstualna polja za username i lozinku

--- app/src/main/java/foi/air/szokpt/ui/LoginPage.kt
+++ app/src/main/java/foi/air/szokpt/ui/LoginPage.kt
@@ -8,17 +8,33 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import foi.air.szokpt.R
+import foi.air.szokpt.ui.components.LoginTextField
+import foi.air.szokpt.ui.theme.BGLevelTwo
 import foi.air.szokpt.ui.theme.BackgroundColor
 
 @Composable
 fun LoginPage() {
+
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -38,6 +54,39 @@ fun LoginPage() {
                 modifier = Modifier
                     .width(200.dp)
             )
+
+            Spacer(modifier = Modifier.height(60.dp))
+
+            Card(
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                colors = CardDefaults.cardColors(containerColor = BGLevelTwo)
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp, vertical = 20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    // polje za username
+                    LoginTextField(
+                        label = "Username",
+                        value = username,
+                        onValueChange = { username = it },
+                        isPasswordField = false,
+                    )
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    // polje za lozinku
+                    LoginTextField(
+                        label = "Password",
+                        value = password,
+                        onValueChange = { password = it },
+                        isPasswordField = true
+                    )
+                }
+            }
         }
     }
 }
