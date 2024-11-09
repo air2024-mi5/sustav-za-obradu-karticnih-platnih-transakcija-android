34e1b1340a2944fca2a1dd483f7d1017ec2a9fbf
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2024-11-09 15:29:21 +0100

    MI5-25 Kreiranje LoginPage datoteke
    Kreirana je nova LoginPage.kt datoteka te je uz osnovnu funkciju LoginPage postavljen i Preview za istu.

---
+++ app/src/main/java/foi/air/szokpt/ui/LoginPage.kt
@@ -0,0 +1,15 @@
+package foi.air.szokpt.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginPage() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPagePreview() {
+    LoginPage()
+}
