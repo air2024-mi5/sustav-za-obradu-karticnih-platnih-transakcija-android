46529b5f1507fa4c0d634a82332f14aa855c5664
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2024-11-09 15:44:09 +0100

    MI5-25 Postavljena pozadina i dodan logo
    Postavljena je pozadina. Dodan je logo tvrtke kao drawable resurs te je ubačen na login zaslon.

--- app/src/main/java/foi/air/szokpt/ui/LoginPage.kt
+++ app/src/main/java/foi/air/szokpt/ui/LoginPage.kt
@@ -1,11 +1,45 @@
 package foi.air.szokpt.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.width
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import foi.air.szokpt.R
+import foi.air.szokpt.ui.theme.BackgroundColor
 
 @Composable
 fun LoginPage() {
-
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BackgroundColor)
+    )
+    {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .width(200.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
