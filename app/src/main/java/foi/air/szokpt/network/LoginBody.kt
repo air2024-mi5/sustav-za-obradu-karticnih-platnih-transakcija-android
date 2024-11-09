a53da338709a8de80fd30d83d3e7237b2b4cdd7b
abernobic21 <126506127+abernobic21@users.noreply.github.com>
2024-11-09 18:31:36 +0100

    MI5-27 Kreirane klase NetworkService, AuthenticationService i dodane potrebne ovisnosti
    NetworkService je singleton klasa za komunikaciju sa serverom (trenutno "http://localhost:8080"). AuthenticationService se koristi za slanje POST zahtjeva na putanju "/login"

---
+++ app/src/main/java/foi/air/szokpt/network/LoginBody.kt
@@ -0,0 +1,6 @@
+package foi.air.szokpt.network
+
+data class LoginBody(
+    val username: String,
+    val password: String
+)
---
+++ app/src/main/java/foi/air/szokpt/network/NetworkService.kt
@@ -0,0 +1,19 @@
+import com.google.gson.GsonBuilder
+import foi.air.szokpt.network.AuthenticationService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkService {
+    private const val BASE_URL = "http://localhost:8080/"
+
+    private val gson = GsonBuilder().setLenient().create()
+
+    private val instance: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+
+
+    val authenticationService: AuthenticationService = instance.create(AuthenticationService::class.java)
+
+}
