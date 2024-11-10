1d9753f97e57c2f14f83dc1dc49368a62162dba7
abernobic21 <126506127+abernobic21@users.noreply.github.com>
2024-11-10 20:50:58 +0100

    MI5-27 Dodane ovisnosti za komunikaciju s localhostom
    Promijenjena IP adresa NetworkService-a na 10.0.2.2 za povezivanje emulatora na localhost računala. network_security_config omogućuje prava za komunikaciju.

--- app/src/main/java/foi/air/szokpt/network/NetworkService.kt
+++ app/src/main/java/foi/air/szokpt/network/NetworkService.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkService {
-    private const val BASE_URL = "http://localhost:8080/"
+    private const val BASE_URL = "http://10.0.2.2:8080/"
 
     private val gson = GsonBuilder().setLenient().create()
 
