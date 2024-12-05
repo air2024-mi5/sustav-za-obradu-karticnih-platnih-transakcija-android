934dfaa941a6ad8df91f8525890614a6a3fa8223
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2024-12-05 21:57:16 +0100

    MI5-49 Kreiran RegistrationOutcomeListener i popravljen RegistrationHandler

--- app/src/main/java/foi/air/szokpt/helpers/RegistrationHandler.kt
+++ app/src/main/java/foi/air/szokpt/helpers/RegistrationHandler.kt
@@ -1,40 +1,31 @@
 package foi.air.szokpt.helpers
 
-import NetworkService
-import foi.air.szokpt.network.RegistrationBody
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import hr.foi.air.core.login.RegistrationBody
+import hr.foi.air.core.network.ResponseListener
+import hr.foi.air.core.network.models.ErrorResponseBody
+import hr.foi.air.core.network.models.SuccessfulResponseBody
+import hr.foi.air.core.register.RegistrationOutcomeListener
+import hr.foi.air.szokpt.ws.models.RegistrationResponse
+import hr.foi.air.szokpt.ws.request_handlers.RegistrationRequestHandler
 
-class RegistrationHandler(
-    private val onSuccessful:(String) -> Unit,
-    private val onFailure:(String) -> Unit,
-    private val setAwaitingResponse:(Boolean) -> Unit
-) {
-    fun register(username: String, password: String, firstName: String, lastName: String, email: String, role: String) {
-        setAwaitingResponse(true)
+class RegistrationHandler() {
+    fun register(registrationBody: RegistrationBody, registrationListener: RegistrationOutcomeListener) {
+        val registrationRequestHandler = RegistrationRequestHandler(registrationBody)
 
-        val service = NetworkService.authenticationService
-        val serviceCall = service.register(RegistrationBody(username, password, firstName, lastName, email, role))
+        registrationRequestHandler.sendRequest(
+            object : ResponseListener<RegistrationResponse> {
+                override fun onSuccessfulResponse(response: SuccessfulResponseBody<RegistrationResponse>) {
+                    registrationListener.onSuccessfulRegistration(response.message)
+                }
 
-        serviceCall.enqueue(object: Callback<String> {
-            override fun onResponse(call: Call<String>?, response: Response<String>?) {
-                if (response != null) {
-                    if (response.isSuccessful) {
-                        onSuccessful("Uspješna registracija!")
-                        //val message = response.body()
-                        //println("Uspjeh: $message")
-                    } else {
-                        onFailure("Neuspješna registracija!")
-                        //println("Greska: ${response.errorBody()?.string()}")
-                    }
-                    setAwaitingResponse(false)
+                override fun onErrorResponse(response: ErrorResponseBody) {
+                    registrationListener.onFailedRegistration(response.message)
+                }
+
+                override fun onNetworkFailure(t: Throwable) {
+                    registrationListener.onFailedRegistration("Could not connect to network.")
                 }
             }
-
-            override fun onFailure(call: Call<String>?, t: Throwable?) {
-                onFailure("Došlo je do greške.")
-            }
-        })
+        )
     }
 }
