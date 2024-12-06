bdac1992d79916dc566e0a16463212ab15c002f2
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2024-12-06 18:23:05 +0100

    MI5-49 Dodano Authorization zaglavlje i refaktoriran RegistrationBody kako bi odgovaralo traženom zahtjevu na API

--- core/src/main/java/hr/foi/air/core/register/RegistrationBody.kt
+++ core/src/main/java/hr/foi/air/core/register/RegistrationBody.kt
@@ -1,9 +1,12 @@
-package hr.foi.air.core.login
+package hr.foi.air.core.register
+
+data class Role(val name: String)
 
 data class RegistrationBody(
     val username: String,
     val password: String,
     val firstName: String,
     val lastName: String,
-    val email: String
+    val email: String,
+    val role: Role
 )
