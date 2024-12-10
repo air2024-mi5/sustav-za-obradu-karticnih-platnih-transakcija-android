5cd9a31081d231a89bee913e17f9b95402b4b716
bkadezabek <bkadezabe20@foi.hr>
2024-12-10 16:24:52 +0100

    MI5-65 Priprema okruženja
    Priprema okruženja za brže testiranje

--- app/src/main/java/foi/air/szokpt/views/MainScreen.kt
+++ app/src/main/java/foi/air/szokpt/views/MainScreen.kt
@@ -44,13 +44,13 @@ fun MainScreen() {
     }
     Scaffold(
         bottomBar = {
-           if(isAuthenticated.value)
-               AnimatedNavigationBar(navController = navController)
+          // if(isAuthenticated.value)
+              AnimatedNavigationBar(navController = navController)
         }
     ) { innerPadding ->
         NavHost(
             navController = navController,
-            startDestination = "login",
+            startDestination = "dashboard",
             modifier = Modifier.padding(innerPadding)
         ) {
             composable("login") { LoginPage(
