50c9fe641cdd6842d3ac349cab3e3116ed20b880
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2024-12-12 16:59:32 +0100

    MI5-41 Izmjena putanje zbog promjene na backendu

--- ws/src/main/java/hr/foi/air/szokpt/ws/network/TransactionsService.kt
+++ ws/src/main/java/hr/foi/air/szokpt/ws/network/TransactionsService.kt
@@ -6,6 +6,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface TransactionsService {
-    @GET("/reports/success")
+    @GET("/reports/successful-transactions")
     fun getTransactionsSuccess(): Call<SuccessfulResponseBody<TransactionsSuccessResponse>>
 }
