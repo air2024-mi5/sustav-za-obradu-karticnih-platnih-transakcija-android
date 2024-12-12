63b08e4e822d89a1ee81d81d9d48134dfd564780
kmilos-foi <kmilos21@foi.hr>
2024-12-12 19:43:46 +0100

    MI5-61 Konfiguriran NetworkService

--- ws/src/main/java/hr/foi/air/szokpt/ws/network/NetworkService.kt
+++ ws/src/main/java/hr/foi/air/szokpt/ws/network/NetworkService.kt
@@ -1,17 +1,25 @@
-import com.google.gson.GsonBuilder
 import hr.foi.air.szokpt.ws.network.AuthenticationService
+import hr.foi.air.szokpt.ws.network.TransactionsService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkService {
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL_ACCOUNTS = "http://10.0.2.2:8080/"
+    private const val BASE_URL_TRANSMNG = "http://10.0.2.2:8081/"
 
-    private val instance: Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
+    private val accountsInstance: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL_ACCOUNTS)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
+    private val transmngInstance: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL_TRANSMNG)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
-    val authenticationService: AuthenticationService = instance.create(AuthenticationService::class.java)
+    val authenticationService: AuthenticationService =
+        accountsInstance.create(AuthenticationService::class.java)
 
+    val transactionsService: TransactionsService =
+        transmngInstance.create(TransactionsService::class.java)
 }
