2f555219ab36543213abcb9909117e99d8d70eee
kmilos-foi <kmilos21@foi.hr>
2024-12-13 23:32:13 +0100

    Ispravljena greška s localhost adresom

--- ws/src/main/java/hr/foi/air/szokpt/ws/network/NetworkConfig.kt
+++ ws/src/main/java/hr/foi/air/szokpt/ws/network/NetworkConfig.kt
@@ -4,10 +4,10 @@ object NetworkConfig {
     private const val USE_LOCAL = false;
 
     val BASE_URL: String
-        get() = if (USE_LOCAL) "http://localhost" else "http://46.202.155.53"
+        get() = if (USE_LOCAL) "http://10.0.2.2" else "http://46.202.155.53"
 
     val ACCOUNT_MNG_PORT: String
-        get() = if (USE_LOCAL) ":8080/" else ":8080/"
+        get() = if (USE_LOCAL) ":8080" else ":8080"
 
     val TRANSACTIONS_MNG_PORT: String
         get() = if (USE_LOCAL) ":8080" else ":8081"
