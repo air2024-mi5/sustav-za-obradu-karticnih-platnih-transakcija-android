eb87984327ec19582f2eef1332bafde23fcf689b
NinoLoncar <nino.loncar16@gmail.com>
2024-12-13 12:43:32 +0100

    Dodana mrežna konfiguracija
    Po defaultu se aplikacija poveziva na pravi server. Ako se želi raditi s localhost, u NetworkConfig treba promijeniti USE_LOCAL i podesiti portove.

--- ws/src/main/java/hr/foi/air/szokpt/ws/network/NetworkService.kt
+++ ws/src/main/java/hr/foi/air/szokpt/ws/network/NetworkService.kt
@@ -1,24 +1,26 @@
 import hr.foi.air.szokpt.ws.network.AuthenticationService
-import hr.foi.air.szokpt.ws.network.TransactionsService
+import hr.foi.air.szokpt.ws.network.NetworkConfig
 import hr.foi.air.szokpt.ws.network.ReportsService
+import hr.foi.air.szokpt.ws.network.TransactionsService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkService {
-    private const val BASE_URL_ACCOUNTS = "http://10.0.2.2:8080/"
-    private const val BASE_URL_TRANSMNG = "http://10.0.2.2:8081/"
-    private const val BASE_URL_REPORTS = "http://10.0.2.2:8082/"
+    private val BASE_URL_ACCOUNT_MNG = NetworkConfig.BASE_URL + NetworkConfig.ACCOUNT_MNG_PORT
+    private val BASE_URL_TRANSACTIONS_MNG =
+        NetworkConfig.BASE_URL + NetworkConfig.TRANSACTIONS_MNG_PORT
+    private val BASE_URL_REPORTS = NetworkConfig.BASE_URL + NetworkConfig.REPORTS_PORT
 
     private val accountsInstance: Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL_ACCOUNTS)
+        .baseUrl(BASE_URL_ACCOUNT_MNG)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
     private val transmngInstance: Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL_TRANSMNG)
+        .baseUrl(BASE_URL_TRANSACTIONS_MNG)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-   
+
     private val reportsInstance: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL_REPORTS)
         .addConverterFactory(GsonConverterFactory.create())
@@ -29,6 +31,6 @@ object NetworkService {
 
     val transactionsService: TransactionsService =
         transmngInstance.create(TransactionsService::class.java)
-    
+
     val reportsService: ReportsService = reportsInstance.create(ReportsService::class.java)
 }
