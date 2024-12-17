aba3394675963706b2c1c61c0f529f174e568694
kmilos-foi <kmilos21@foi.hr>
2024-12-17 19:14:47 +0100

    MI5-69 Kreirana metoda za dohvat svih korisnika

--- ws/src/main/java/hr/foi/air/szokpt/ws/network/UsersService.kt
+++ ws/src/main/java/hr/foi/air/szokpt/ws/network/UsersService.kt
@@ -3,8 +3,10 @@ package hr.foi.air.szokpt.ws.network
 import hr.foi.air.core.network.models.SuccessfulResponseBody
 import hr.foi.air.core.register.RegistrationBody
 import hr.foi.air.szokpt.ws.models.RegistrationResponse
+import hr.foi.air.szokpt.ws.models.responses.User
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
@@ -14,4 +16,9 @@ interface UsersService {
         @Header("Authorization") authHeader: String,
         @Body registrationBody: RegistrationBody
     ): Call<SuccessfulResponseBody<RegistrationResponse>>
+
+    @GET("/users")
+    fun getUsers(
+        @Header("Authorization") authHeader: String,
+    ): Call<SuccessfulResponseBody<User>>
 }
