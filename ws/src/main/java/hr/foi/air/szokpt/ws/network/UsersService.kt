267536d5925458620b51fd69ea149da92be1e977
kmilos-foi <kmilos21@foi.hr>
2024-12-17 19:12:50 +0100

    MI5-69 Kreiran UsersService
    .Metoda za registraciju prebačena iz AuthenticationServicea u UsersService jer registracija ne spada u autentikaciju, već u upravljanje korisnicima.

---
+++ ws/src/main/java/hr/foi/air/szokpt/ws/network/UsersService.kt
@@ -0,0 +1,17 @@
+package hr.foi.air.szokpt.ws.network
+
+import hr.foi.air.core.network.models.SuccessfulResponseBody
+import hr.foi.air.core.register.RegistrationBody
+import hr.foi.air.szokpt.ws.models.RegistrationResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface UsersService {
+    @POST("/register")
+    fun register(
+        @Header("Authorization") authHeader: String,
+        @Body registrationBody: RegistrationBody
+    ): Call<SuccessfulResponseBody<RegistrationResponse>>
+}
