0ee2ea46bb7f98a9279d9092a2dc1f27d7ed8c99
abernobic21 <126506127+abernobic21@users.noreply.github.com>
2024-12-20 18:21:42 +0100

    MI5-85 Dodana metoda za slanje zahtjeva za djelomično ažuriranje transakcije
    U TransactionsService dodana putanja za slanje Put zahtjeva na putanju transactions/{id}

--- ws/src/main/java/hr/foi/air/szokpt/ws/network/TransactionsService.kt
+++ ws/src/main/java/hr/foi/air/szokpt/ws/network/TransactionsService.kt
@@ -2,9 +2,12 @@ package hr.foi.air.szokpt.ws.network
 
 import hr.foi.air.core.network.models.SuccessfulResponseBody
 import hr.foi.air.szokpt.ws.models.TransactionPageResponse
+import hr.foi.air.szokpt.ws.models.responses.Transaction
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 interface TransactionsService {
@@ -12,5 +15,12 @@ interface TransactionsService {
     fun getTransactionPage(
         @Header("Authorization") authHeader: String,
         @Query("page") page: Int,
+    ): Call<SuccessfulResponseBody<TransactionPageResponse>>
+
+    @PUT("transactions/{id}")
+    fun updateTransaction(
+        @Header("Authorization") authHeader: String,
+        @Query("id") id: Int,
+        @Body transaction: Transaction
     ): Call<SuccessfulResponseBody<TransactionPageResponse>>
 }
