0c1a9471df615495992385daa6fe2fb9088cb3a8
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2024-12-27 12:44:37 +0100

    MI5-55 Dodani osnovni elementi o transakciji kao zaglavlje
    U "zaglavlje" su dodani osnovni elementi poput broja transakcije, iznosa i valute.

--- app/src/main/java/foi/air/szokpt/views/app/TransactionDetailsView.kt
+++ app/src/main/java/foi/air/szokpt/views/app/TransactionDetailsView.kt
@@ -1,7 +1,36 @@
 package foi.air.szokpt.views.app
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import foi.air.szokpt.R
+import foi.air.szokpt.ui.components.TileSegment
+import foi.air.szokpt.ui.theme.BGLevelOne
+import foi.air.szokpt.ui.theme.Primary
+import foi.air.szokpt.ui.theme.TextWhite
+import foi.air.szokpt.ui.theme.TileSizeMode
 import hr.foi.air.szokpt.ws.models.responses.Transaction
 
 @Composable
@@ -9,5 +38,124 @@ fun TransactionDetailsView(
     navController: NavController,
     transaction: Transaction
 ) {
-    
+    val currencySymbol = when (transaction.currency) {
+        "840" -> "$"
+        "978" -> "€"
+        else -> ""
+    }
+
+    val trxTypeMap = mapOf(
+        "sale" to "Sale",
+        "refund" to "Refund",
+        "void_sale" to "Void sale",
+        "void_refund" to "Void refund",
+        "reversal_sale" to "Reversal sale",
+        "reversal_refund" to "Reversal refund"
+    )
+
+    val cardBrandDrawable = when (transaction.cardBrand) {
+        "Maestro" -> R.drawable.maestro
+        "Visa" -> R.drawable.visa
+        "MasterCard" -> R.drawable.mastercard
+        "Diners" -> R.drawable.diners
+        "Discover" -> R.drawable.discover
+        else -> R.drawable.logo
+    }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            modifier = Modifier.padding(16.dp),
+            text = "Transaction Details",
+            color = TextWhite,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            item {
+                TileSegment(
+                    tileSizeMode = TileSizeMode.WRAP_CONTENT,
+                    innerPadding = 8.dp,
+                    outerMargin = 0.dp,
+                    minWidth = 250.dp,
+                    minHeight = 90.dp,
+                    color = Color.Transparent
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(30.dp))
+                            .background(
+                                Brush.verticalGradient(
+                                    colors = listOf(BGLevelOne, Primary),
+                                    startY = 0f,
+                                    endY = 1600f
+                                )
+                            )
+                            .fillMaxSize()
+                            .padding(16.dp)
+                    ) {
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(8.dp),
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Rounded.Info,
+                                    contentDescription = null,
+                                    tint = Color.White,
+                                    modifier = Modifier.size(60.dp)
+                                )
+                            }
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 8.dp)
+                            ) {
+                                Box(
+                                    modifier = Modifier
+                                        .background(
+                                            color = BGLevelOne,
+                                            shape = RoundedCornerShape(30.dp)
+                                        )
+                                        .padding(horizontal = 12.dp, vertical = 8.dp)
+                                ) {
+                                    Text(
+                                        text = "Transaction #${transaction.id}",
+                                        color = Primary,
+                                        fontWeight = FontWeight.SemiBold,
+                                        fontSize = 18.sp
+                                    )
+                                }
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier.padding(10.dp)
+                                ) {
+                                    Text(
+                                        text = currencySymbol,
+                                        color = Color.White,
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 18.sp,
+                                        modifier = Modifier.padding(end = 4.dp)
+                                    )
+                                    Text(
+                                        text = "${transaction.amount}",
+                                        color = TextWhite,
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 18.sp
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
