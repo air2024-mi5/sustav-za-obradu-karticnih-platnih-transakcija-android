5d4a71ad65519ad0c3117a0d7b63c7cbfdf554b4
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2024-12-27 11:15:06 +0100

    MI5-55 Kreiran TransactionDetailsView

---
+++ app/src/main/java/foi/air/szokpt/views/app/TransactionDetailsView.kt
@@ -0,0 +1,13 @@
+package foi.air.szokpt.views.app
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import hr.foi.air.szokpt.ws.models.responses.Transaction
+
+@Composable
+fun TransactionDetailsView(
+    navController: NavController,
+    transaction: Transaction
+) {
+    
+}
