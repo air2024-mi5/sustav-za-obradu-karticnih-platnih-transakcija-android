241d9e2c6ecd5e0abbfafed6398db58c1c1afc05
kmilos-foi <kmilos21@foi.hr>
2025-01-14 23:13:58 +0100

    MI5-45 Izrada metode za upis odabranih transakcija

--- ws/src/main/java/hr/foi/air/szokpt/ws/network/ProcessingService.kt
+++ ws/src/main/java/hr/foi/air/szokpt/ws/network/ProcessingService.kt
@@ -2,13 +2,22 @@ package hr.foi.air.szokpt.ws.network
 
 import hr.foi.air.szokpt.core.network.models.SuccessfulResponseBody
 import hr.foi.air.szokpt.ws.models.responses.SelectedTransaction
+import hr.foi.air.szokpt.ws.models.responses.SelectedTransactions
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface ProcessingService {
     @GET("/selected-transactions")
     fun getSelectedTransactions(
         @Header("Authorization") authHeader: String
     ): Call<SuccessfulResponseBody<SelectedTransaction>>
+
+    @POST("selected-transactions")
+    fun addSelectedTransactions(
+        @Header("Authorization") authHeader: String,
+        @Body body: SelectedTransactions
+    ): Call<SuccessfulResponseBody<Unit>>
 }
