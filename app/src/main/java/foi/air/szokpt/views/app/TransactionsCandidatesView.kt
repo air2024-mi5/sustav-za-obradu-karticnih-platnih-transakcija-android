3a8644aa3c58e2f58259e2d909d4edb29e012ed3
kmilos-foi <kmilos21@foi.hr>
2025-01-14 18:18:18 +0100

    MI5-45 Premješten gumb za odabir svih transakcija
    Gumb je premješten u zasebni composable file.

--- app/src/main/java/foi/air/szokpt/views/app/TransactionsCandidatesView.kt
+++ app/src/main/java/foi/air/szokpt/views/app/TransactionsCandidatesView.kt
@@ -1,23 +1,13 @@
 package foi.air.szokpt.views.app
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Check
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
@@ -25,20 +15,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import foi.air.szokpt.ui.components.processing_components.transactionsCandidatesView.SelectAllTransactionsButton
 import foi.air.szokpt.ui.components.processing_components.transactionsCandidatesView.SelectCandidatesButton
 import foi.air.szokpt.ui.components.processing_components.transactionsCandidatesView.TransactionCandidateItem
 import foi.air.szokpt.ui.theme.Primary
 import foi.air.szokpt.ui.theme.TextBlack
 import foi.air.szokpt.viewmodels.TransactionsCandidatesViewModel
-import hr.foi.air.szokpt.ws.models.responses.Transaction
 import kotlinx.coroutines.launch
 
 @Composable
@@ -75,7 +61,7 @@ fun TransactionsCandidatesView(
 
     Column(modifier = Modifier.fillMaxSize()) {
 
-        SelectAllButton(
+        SelectAllTransactionsButton(
             transactionPage?.transactions,
             viewModel,
             iconTintColor
@@ -105,52 +91,6 @@ fun TransactionsCandidatesView(
             horizontalArrangement = Arrangement.Center,
         ) {
             SelectCandidatesButton()
-        }
-    }
-}
-
-@Composable
-fun SelectAllButton(
-    transactions: List<Transaction>?,
-    viewModel: TransactionsCandidatesViewModel,
-    iconTintColor: Color
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        horizontalArrangement = Arrangement.End,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = "Select all",
-            color = Primary,
-            fontWeight = FontWeight.SemiBold,
-            fontSize = 18.sp,
-            modifier = Modifier.padding(end = 8.dp)
-        )
-
-        Button(
-            onClick = {
-                transactions?.let { transactions ->
-                    val pageGuids = transactions.map { it.guid }.toSet()
-                    viewModel.toggleSelectAllTransactions(pageGuids)
-                }
-            },
-            modifier = Modifier
-                .size(24.dp),
-            shape = RoundedCornerShape(4.dp),
-            border = BorderStroke(1.dp, Primary),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Transparent
-            ),
-            contentPadding = PaddingValues(0.dp)
-        ) {
-            Icon(
-                imageVector = Icons.Outlined.Check,
-                contentDescription = "",
-                tint = iconTintColor,
-            )
         }
     }
 }
