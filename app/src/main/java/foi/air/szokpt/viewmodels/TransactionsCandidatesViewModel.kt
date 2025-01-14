e9e8c11acb5c4cf076075d88a8f1c00130f621af
kmilos-foi <kmilos21@foi.hr>
2025-01-14 18:14:37 +0100

    MI5-45 Kreiran TransactionsCandidatesViewModel
    Ovaj view model sadrži metodu za dohvat transakcija (tj. stranice s transakcijama). Sadrži i funkciju za upravljanje listom odabranih transakcija te za ažuriranje pojedinačne promjene označenosti.

---
+++ app/src/main/java/foi/air/szokpt/viewmodels/TransactionsCandidatesViewModel.kt
@@ -0,0 +1,80 @@
+package foi.air.szokpt.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import foi.air.szokpt.context.Auth
+import hr.foi.air.szokpt.core.network.ResponseListener
+import hr.foi.air.szokpt.core.network.models.ErrorResponseBody
+import hr.foi.air.szokpt.core.network.models.SuccessfulResponseBody
+import hr.foi.air.szokpt.core.transactions.TransactionFilter
+import hr.foi.air.szokpt.ws.models.TransactionPageResponse
+import hr.foi.air.szokpt.ws.request_handlers.GetTransactionsPageRequestHandler
+import java.util.UUID
+
+class TransactionsCandidatesViewModel : ViewModel() {
+    private val _transactionPage: MutableLiveData<TransactionPageResponse> = MutableLiveData(null)
+    private val _currentPage: MutableLiveData<Int> = MutableLiveData(1)
+    private val _totalPages: MutableLiveData<Int> = MutableLiveData(1)
+    private val _selectedGuids: MutableLiveData<List<UUID>> = MutableLiveData(mutableListOf())
+
+    val transactionPage: LiveData<TransactionPageResponse?> = _transactionPage
+    val currentPage: LiveData<Int> = _currentPage
+    val totalPages: LiveData<Int> = _totalPages
+    val selectedGuids: LiveData<List<UUID>> = _selectedGuids
+
+    fun fetchTransactionPage(page: Int) {
+        val jwtToken = Auth.logedInUserData?.token ?: return
+
+        val transactionsRequestHandler =
+            GetTransactionsPageRequestHandler(jwtToken, page, filter = setFilter())
+        transactionsRequestHandler.sendRequest(object : ResponseListener<TransactionPageResponse> {
+            override fun onSuccessfulResponse(response: SuccessfulResponseBody<TransactionPageResponse>) {
+                val transactionPage = response.data
+                _transactionPage.value = transactionPage?.first()
+                _currentPage.value = _transactionPage.value?.currentPage
+                _totalPages.value = _transactionPage.value?.totalPages
+            }
+
+            override fun onErrorResponse(response: ErrorResponseBody) {
+                println("Error receiving response: ${response.error_message}")
+            }
+
+            override fun onNetworkFailure(t: Throwable) {
+                println("Error contacting network...")
+            }
+        })
+    }
+
+    fun toggleSelectAllTransactions(pageGuids: Set<UUID>) {
+        _selectedGuids.value = _selectedGuids.value.orEmpty().let { currentSelectedGuids ->
+            if (pageGuids.all { it in currentSelectedGuids }) {
+                currentSelectedGuids - pageGuids
+            } else {
+                currentSelectedGuids + pageGuids
+            }
+        }
+    }
+
+    fun updateSelectionStatus(transactionId: UUID, isSelected: Boolean) {
+        _selectedGuids.value = _selectedGuids.value.orEmpty().let { currentSelectedGuids ->
+            if (isSelected) {
+                currentSelectedGuids + transactionId
+            } else {
+                currentSelectedGuids - transactionId
+            }
+        }
+    }
+
+    fun setFilter(): TransactionFilter {
+        return TransactionFilter(
+            cardBrands = emptyList(),
+            trxTypes = emptyList(),
+            minAmount = null,
+            maxAmount = null,
+            afterDate = null,
+            beforeDate = null,
+            processed = false
+        )
+    }
+}
