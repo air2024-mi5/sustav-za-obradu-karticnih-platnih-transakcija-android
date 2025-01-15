3bf21b757eb5689ef88ce8bd43e62a3fa20d50ef
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2025-01-15 13:30:03 +0100

    MI5-100 Implementirana funkcija za crtanje grafova prijema dobivenim vrijednostima

---
+++ app/src/main/java/foi/air/szokpt/ui/components/dashboard_components/CardBrandsBarChart.kt
@@ -0,0 +1,69 @@
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.unit.dp
+import foi.air.szokpt.models.CardBrandInformation
+
+@Composable
+fun CardBrandsBarChart(
+    modifier: Modifier = Modifier,
+    stats: List<CardBrandInformation>,
+) {
+    val maxCount = stats.maxOfOrNull { it.count } ?: 0
+    if (maxCount == 0) return
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+        ) {
+            var totalWidth by remember { mutableFloatStateOf(0f) }
+            var availableWidth by remember { mutableFloatStateOf(0f) }
+            var barWidth by remember { mutableFloatStateOf(0f) }
+            val spacing = 50f
+
+            Canvas(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(130.dp)
+                    .padding(bottom = 10.dp)
+            ) {
+                val minBarWidth = 40f
+                totalWidth = size.width
+                availableWidth = totalWidth - (spacing * (stats.size))
+                barWidth = (availableWidth / stats.size).coerceAtLeast(minBarWidth)
+
+                stats.forEachIndexed { index, stat ->
+                    if (stat.count != 0) {
+                        val x = index * (barWidth + spacing) + (spacing / 2)
+                        val height = ((stat.count.toFloat() / maxCount) * (size.height - 20f))
+
+                        drawRect(
+                            color = stat.color,
+                            topLeft = Offset(x, size.height - height - 20f),
+                            size = Size(barWidth, height),
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+
