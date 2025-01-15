78654feca2c2621890857197a46daeafab69162e
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2025-01-15 13:31:55 +0100

    MI5-100 Kao legenda grafu dodane su slike kartičnih brandova

--- app/src/main/java/foi/air/szokpt/ui/components/dashboard_components/CardBrandsBarChart.kt
+++ app/src/main/java/foi/air/szokpt/ui/components/dashboard_components/CardBrandsBarChart.kt
@@ -1,24 +1,32 @@
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import foi.air.szokpt.helpers.TransactionUtils
 import foi.air.szokpt.models.CardBrandInformation
 
 @Composable
 fun CardBrandsBarChart(
     modifier: Modifier = Modifier,
     stats: List<CardBrandInformation>,
+    transactionUtils: TransactionUtils
 ) {
     val maxCount = stats.maxOfOrNull { it.count } ?: 0
     if (maxCount == 0) return
@@ -58,6 +66,31 @@ fun CardBrandsBarChart(
                             color = stat.color,
                             topLeft = Offset(x, size.height - height - 20f),
                             size = Size(barWidth, height),
+                        )
+                    }
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomStart),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                stats.forEach { stat ->
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.width((barWidth / 2).dp)
+
+
+                    ) {
+                        Image(
+                            painter = painterResource(
+                                id = transactionUtils.getCardBrandDrawable(stat.brand)
+                            ),
+                            contentDescription = "Card ${stat.brand}",
+                            modifier = Modifier
+                                .width(35.2.dp)
                         )
                     }
                 }
