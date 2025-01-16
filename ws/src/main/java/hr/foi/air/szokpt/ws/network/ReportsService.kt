3b994891114e4dc6acffa4631f18cb581759056d
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2025-01-16 17:06:53 +0100

    MI5-102 Dodana definicija GET zahtjeva
    U ReportsService dodana definicija GET zahtjeva prema putanji na serveru za dohvat podataka o kartičnim brendovima, pomoću funkcije getCardBrandsStatistics().

--- ws/src/main/java/hr/foi/air/szokpt/ws/network/ReportsService.kt
+++ ws/src/main/java/hr/foi/air/szokpt/ws/network/ReportsService.kt
@@ -1,6 +1,7 @@
 package hr.foi.air.szokpt.ws.network
 
 import hr.foi.air.szokpt.core.network.models.SuccessfulResponseBody
+import hr.foi.air.szokpt.ws.models.CardBrandsStatisticsResponse
 import hr.foi.air.szokpt.ws.models.TransactionsSuccessResponse
 import retrofit2.Call
 import retrofit2.http.GET
@@ -8,4 +9,7 @@ import retrofit2.http.GET
 interface ReportsService {
     @GET("/reports/successful-transactions")
     fun getTransactionsSuccess(): Call<SuccessfulResponseBody<TransactionsSuccessResponse>>
+
+    @GET("/reports/card-brands")
+    fun getCardBrandsStatistics(): Call<SuccessfulResponseBody<CardBrandsStatisticsResponse>>
 }
