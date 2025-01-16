449e622203a88eccb25cfa0abe5cc1c7ad97d4c3
abernobic21 <126506127+abernobic21@users.noreply.github.com>
2025-01-16 14:27:52 +0100

    Ažuriran TransactionViewModel

--- app/src/main/java/foi/air/szokpt/viewmodels/TransactionViewModel.kt
+++ app/src/main/java/foi/air/szokpt/viewmodels/TransactionViewModel.kt
@@ -4,28 +4,51 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import foi.air.szokpt.context.Auth
+import foi.air.szokpt.helpers.TransactionDetailsHandler
 import foi.air.szokpt.helpers.TransactionUpdateHandler
 import foi.air.szokpt.utils.TransactionDataValidator
+import hr.foi.air.szokpt.core.transactions.TransactionData
+import hr.foi.air.szokpt.core.transactions.TransactionDetailsOutcomeListener
 import hr.foi.air.szokpt.core.transactions.TransactionUpdateOutcomeListener
 import hr.foi.air.szokpt.ws.models.responses.Transaction
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.UUID
 
 class TransactionViewModel() : ViewModel() {
-    private val _storedTransactionData : MutableLiveData<Transaction> = MutableLiveData()
-    private val _currentTransactionData : MutableLiveData<Transaction> = MutableLiveData()
+    private val _storedTransactionData : MutableLiveData<TransactionData?> = MutableLiveData()
+    private val _currentTransactionData : MutableLiveData<TransactionData?> = MutableLiveData()
     private val _message : MutableLiveData<String> = MutableLiveData("")
 
-    val storedTransactionData : LiveData<Transaction> = _storedTransactionData
-    val currentTransactionData : LiveData<Transaction> = _currentTransactionData
+    val storedTransactionData : LiveData<TransactionData?> = _storedTransactionData
+    val currentTransactionData : LiveData<TransactionData?> = _currentTransactionData
     val message : LiveData<String> = _message
 
-    fun initializeTransactionData(transaction: Transaction) {
-        _storedTransactionData.value = transaction
-        _currentTransactionData.value = transaction
+    fun initializeTransactionData(transactionGuid: UUID) {
+        val jwtToken = Auth.logedInUserData?.token ?: return
+        val transactionDetailsHandler = TransactionDetailsHandler()
+
+        transactionDetailsHandler.getTransactionDetails(
+            jwtToken,
+            transactionGuid,
+            object : TransactionDetailsOutcomeListener {
+            override fun onSuccessfulTransactionDetailsFetch(transactionData: TransactionData) {
+                _storedTransactionData.value = transactionData
+                _currentTransactionData.value = _storedTransactionData.value
+                _message.value = ""
+            }
+
+            override fun onFailedTransactionDetailsFetch(failureMessage: String) {
+                _message.value = failureMessage
+                _storedTransactionData.value = null
+                _currentTransactionData.value = null
+            }
+        })
     }
 
     fun updateTransactionData(
         transactionUpdateHandler: TransactionUpdateHandler,
-        newTransactionData: Transaction
+        newTransactionData: TransactionData
     ) {
         val jwtToken = Auth.logedInUserData?.token
         if(jwtToken == null) {
@@ -47,7 +70,7 @@ class TransactionViewModel() : ViewModel() {
         })
     }
 
-    fun validateData(transaction: Transaction) : Boolean{
+    fun validateData(transaction: TransactionData) : Boolean{
         val validator = TransactionDataValidator()
         val validationResult = validator.validate(transaction)
 
@@ -74,6 +97,10 @@ class TransactionViewModel() : ViewModel() {
 
     fun updateTimestamp(timestamp: String) {
         _currentTransactionData.value = _currentTransactionData.value?.copy(transactionTimestamp = timestamp)
+    }
+
+    fun setMessage(message: String) {
+        _message.value = message
     }
 
     fun clearMessage() {
