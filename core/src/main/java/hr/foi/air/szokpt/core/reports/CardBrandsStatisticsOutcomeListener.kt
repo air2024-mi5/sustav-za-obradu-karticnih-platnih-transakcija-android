375d6bea63f6fedfba2311a2643a50ea037f2bf6
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2025-01-17 12:27:57 +0100

    MI5-102 Podaci o kartičnim brandovima izdvojeni u zasebnu data klasu

--- core/src/main/java/hr/foi/air/szokpt/core/reports/CardBrandsStatisticsOutcomeListener.kt
+++ core/src/main/java/hr/foi/air/szokpt/core/reports/CardBrandsStatisticsOutcomeListener.kt
@@ -2,13 +2,7 @@ package hr.foi.air.szokpt.core.reports
 
 interface CardBrandsStatisticsOutcomeListener {
     fun onSuccessfulCardBrandsStatisticsFetch(
-        totalTransactions: Int,
-        visaCount: Int,
-        dinersCount: Int,
-        discoverCount: Int,
-        maestroCount: Int,
-        amexCount: Int,
-        mastercardCount: Int,
+        cardBrandsStatisticsData: CardBrandsStatisticsData
     )
 
     fun onFailedCardBrandsStatisticsFetch(
