283c181928af2f132efcc92c63ef26e912c0873c
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2025-01-17 12:19:43 +0100

    MI5-102 Kreiran CardBrandsStatisticsHandler

---
+++ app/src/main/java/foi/air/szokpt/helpers/CardBrandsStatisticsHandler.kt
@@ -0,0 +1,43 @@
+package foi.air.szokpt.helpers
+
+import android.util.Log
+import hr.foi.air.szokpt.core.network.ResponseListener
+import hr.foi.air.szokpt.core.network.models.ErrorResponseBody
+import hr.foi.air.szokpt.core.network.models.SuccessfulResponseBody
+import hr.foi.air.szokpt.core.reports.CardBrandsStatisticsOutcomeListener
+import hr.foi.air.szokpt.ws.models.CardBrandsStatisticsResponse
+import hr.foi.air.szokpt.ws.request_handlers.CardBrandsStatisticsRequestHandler
+
+class CardBrandsStatisticsHandler() {
+    fun getTransactionDetails(
+        cardBrandsStatisticsOutcomeListener: CardBrandsStatisticsOutcomeListener
+    ) {
+        val cardBrandsStatisticsRequestHandler = CardBrandsStatisticsRequestHandler()
+
+        cardBrandsStatisticsRequestHandler.sendRequest(
+            object : ResponseListener<CardBrandsStatisticsResponse> {
+                override fun onSuccessfulResponse(response: SuccessfulResponseBody<CardBrandsStatisticsResponse>) {
+                    Log.d("CARDBRANDS", "Response je: ${response.data}")
+                }
+
+                override fun onErrorResponse(response: ErrorResponseBody) {
+                    cardBrandsStatisticsOutcomeListener.onFailedCardBrandsStatisticsFetch(
+                        "An error occurred while fetching card brands statistics: $response.message"
+                            ?: "An error occurred while fetching card brands statistics."
+                    )
+                    Log.d("CARDBRANDS", "An error occurred while fetching card brands statistics.")
+                }
+
+                override fun onNetworkFailure(t: Throwable) {
+                    cardBrandsStatisticsOutcomeListener.onFailedCardBrandsStatisticsFetch(
+                        "Network error: $t.message" ?: "A network error occurred."
+                    )
+                    Log.d(
+                        "CARDBRANDS",
+                        "A NETWORK error occurred while fetching card brands statistics."
+                    )
+                }
+            }
+        )
+    }
+}
