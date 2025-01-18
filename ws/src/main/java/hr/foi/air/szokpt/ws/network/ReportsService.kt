2e48105cbb68c39b19929d18eaa54ba5948ceb5e
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2025-01-18 23:47:55 +0100

    MI5-99 U ReportsService dodana definicija GET zahtjeva

--- ws/src/main/java/hr/foi/air/szokpt/ws/network/ReportsService.kt
+++ ws/src/main/java/hr/foi/air/szokpt/ws/network/ReportsService.kt
@@ -1,11 +1,16 @@
 package hr.foi.air.szokpt.ws.network
 
 import hr.foi.air.szokpt.core.network.models.SuccessfulResponseBody
+import hr.foi.air.szokpt.ws.models.TransactionsPerDayResponse
 import hr.foi.air.szokpt.ws.models.TransactionsSuccessResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface ReportsService {
     @GET("/reports/successful-transactions")
     fun getTransactionsSuccess(): Call<SuccessfulResponseBody<TransactionsSuccessResponse>>
+
+    @GET("/reports/successful-transactions")
+    fun getTransactionsPerDay(@Header("Authorization") authHeader: String): Call<SuccessfulResponseBody<TransactionsPerDayResponse>>
 }
