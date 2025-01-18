1eb5a1e9af910a3c3f9716e85bf4d7626716f222
kmilos-foi <kmilos21@foi.hr>
2025-01-18 22:52:30 +0100

    MI5-45 Izmjena poruke o selekciji kandidata

--- app/src/main/java/foi/air/szokpt/ui/components/processing_components/transactionsCandidatesView/SelectTransactionsDialog.kt
+++ app/src/main/java/foi/air/szokpt/ui/components/processing_components/transactionsCandidatesView/SelectTransactionsDialog.kt
@@ -1,7 +1,7 @@
 package foi.air.szokpt.ui.components.processing_components.transactionsCandidatesView
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import foi.air.szokpt.ui.components.dialog_components.DialogComponent
@@ -22,28 +22,10 @@ fun SelectTransactionsDialog(
         },
         onDismissRequest = { openAddCandidatesDialog.value = false },
         dialogTitle = dialogTitle,
-        dialogText = "Are you sure you want to confirm the selection of transactions for daily processing?\n",
-        iconTop = Icons.Rounded.Close,
+        dialogText = "Are you sure you want to confirm the selection for daily processing?\n",
+        iconTop = Icons.Rounded.Check,
         highlightColor = warning,
         containerColor = BGLevelTwo,
         titleColor = TextWhite,
     )
 }
-
-
-/*
-    DialogComponent(
-        onConfirmation = {
-            onConfirm()
-            openBlockDialog.value = false
-        },
-        onDismissRequest = { openBlockDialog.value = false },
-        dialogTitle = dialogTitle,
-        dialogText = "Are you sure you want to $dialogTextBlockedStatus ${user.firstName} ${user.lastName}, ${user.username}? \n",
-        iconTop = Icons.Rounded.Close,
-        highlightColor = warning,
-        containerColor = BGLevelTwo,
-        titleColor = TextWhite,
-    )
-}
- */
