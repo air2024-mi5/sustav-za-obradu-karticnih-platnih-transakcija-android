6245401bd1a342e0b155ee809f7c9272ec7373fd
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2025-01-18 21:31:01 +0100

    MI5-102 Obrada uspješnog odgovora i mapiranje podataka u CardBrandsStatisticsData

--- app/src/main/java/foi/air/szokpt/helpers/CardBrandsStatisticsHandler.kt
+++ app/src/main/java/foi/air/szokpt/helpers/CardBrandsStatisticsHandler.kt
@@ -1,9 +1,9 @@
 package foi.air.szokpt.helpers
 
-import android.util.Log
 import hr.foi.air.szokpt.core.network.ResponseListener
 import hr.foi.air.szokpt.core.network.models.ErrorResponseBody
 import hr.foi.air.szokpt.core.network.models.SuccessfulResponseBody
+import hr.foi.air.szokpt.core.reports.CardBrandsStatisticsData
 import hr.foi.air.szokpt.core.reports.CardBrandsStatisticsOutcomeListener
 import hr.foi.air.szokpt.ws.models.CardBrandsStatisticsResponse
 import hr.foi.air.szokpt.ws.request_handlers.CardBrandsStatisticsRequestHandler
@@ -18,6 +18,24 @@ class CardBrandsStatisticsHandler() {
         cardBrandsStatisticsRequestHandler.sendRequest(
             object : ResponseListener<CardBrandsStatisticsResponse> {
                 override fun onSuccessfulResponse(response: SuccessfulResponseBody<CardBrandsStatisticsResponse>) {
+                    if (response.data != null && response.data!!.isNotEmpty()) {
+                        val statistics = response.data!![0]
+                        cardBrandsStatisticsOutcomeListener.onSuccessfulCardBrandsStatisticsFetch(
+                            cardBrandsStatisticsData = CardBrandsStatisticsData(
+                                totalTransactions = statistics.totalTransactions,
+                                visaCount = statistics.visaCount,
+                                dinersCount = statistics.dinersCount,
+                                discoverCount = statistics.discoverCount,
+                                maestroCount = statistics.maestroCount,
+                                amexCount = statistics.amexCount,
+                                mastercardCount = statistics.mastercardCount
+                            )
+                        )
+                    } else {
+                        cardBrandsStatisticsOutcomeListener.onFailedCardBrandsStatisticsFetch(
+                            "No card brands statistics data available."
+                        )
+                    }
                 }
 
                 override fun onErrorResponse(response: ErrorResponseBody) {
@@ -25,16 +43,11 @@ class CardBrandsStatisticsHandler() {
                         "An error occurred while fetching card brands statistics: ${response.message}"
                             ?: "An error occurred while fetching card brands statistics."
                     )
-                    Log.d("CARDBRANDS", "An error occurred while fetching card brands statistics.")
                 }
 
                 override fun onNetworkFailure(t: Throwable) {
                     cardBrandsStatisticsOutcomeListener.onFailedCardBrandsStatisticsFetch(
                         "Network error: $t.message" ?: "A network error occurred."
-                    )
-                    Log.d(
-                        "CARDBRANDS",
-                        "A NETWORK error occurred while fetching card brands statistics."
                     )
                 }
             }
