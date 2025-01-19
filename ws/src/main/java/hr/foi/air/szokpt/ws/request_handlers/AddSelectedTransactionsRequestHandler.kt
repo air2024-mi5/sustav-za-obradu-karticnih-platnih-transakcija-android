3d1a0efd340cea885c66e67b1b09e014cc2689c4
kmilos-foi <kmilos21@foi.hr>
2025-01-19 18:21:49 +0100

    MI5-45 Ispravljeno ime AddSelectedTransactions
    AddSelectedTransactions se sada zove AddSelectedTransactionsRequestHandler

--- ws/src/main/java/hr/foi/air/szokpt/ws/request_handlers/AddSelectedTransactionsRequestHandler.kt
+++ ws/src/main/java/hr/foi/air/szokpt/ws/request_handlers/AddSelectedTransactionsRequestHandler.kt
@@ -5,7 +5,7 @@ import hr.foi.air.szokpt.core.network.models.SuccessfulResponseBody
 import hr.foi.air.szokpt.core.transactions.SelectedTransactions
 import retrofit2.Call
 
-class AddSelectedTransactions(
+class AddSelectedTransactionsRequestHandler(
     private val jwtToken: String,
     private val guids: SelectedTransactions
 ) : TemplateRequestHandler<Unit>() {
