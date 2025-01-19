072e6d415baccd2a6304bcbfa38fec0b11e71bda
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2025-01-19 02:07:33 +0100

    MI5-99 Mapiranje podataka iz odgovora

--- ws/src/main/java/hr/foi/air/szokpt/ws/models/TransactionsPerDayResponse.kt
+++ ws/src/main/java/hr/foi/air/szokpt/ws/models/TransactionsPerDayResponse.kt
@@ -1,9 +1,9 @@
 package hr.foi.air.szokpt.ws.models
 
 import com.google.gson.annotations.SerializedName
-import java.sql.Timestamp
+
 
 data class TransactionsPerDayResponse(
     @SerializedName("totalTransactions") val totalTransactions: Int,
-    @SerializedName("transactionsPerDay") val transactionsPerDay: Map<Timestamp, Int>
+    @SerializedName("transactionsPerDay") val transactionsPerDay: Map<String, Int>
 )
