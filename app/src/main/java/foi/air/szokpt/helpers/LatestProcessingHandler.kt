821437d7adf36e4e552cd23d038ecdccf2947ed8
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2025-01-20 15:48:44 +0100

    MI5-96 Dorada LatestProcessingHandler-a

--- app/src/main/java/foi/air/szokpt/helpers/LatestProcessingHandler.kt
+++ app/src/main/java/foi/air/szokpt/helpers/LatestProcessingHandler.kt
@@ -1,11 +1,11 @@
 package foi.air.szokpt.helpers
 
-import android.util.Log
 import hr.foi.air.szokpt.core.network.ResponseListener
 import hr.foi.air.szokpt.core.network.models.ErrorResponseBody
 import hr.foi.air.szokpt.core.network.models.SuccessfulResponseBody
 import hr.foi.air.szokpt.core.processing.LatestProcessingOutcomeListener
 import hr.foi.air.szokpt.ws.models.LatestProcessingResponse
+import hr.foi.air.szokpt.ws.models.responses.toCoreBatchRecord
 import hr.foi.air.szokpt.ws.request_handlers.LatestProcessingRequestHandler
 
 class LatestProcessingHandler {
@@ -19,11 +19,16 @@ class LatestProcessingHandler {
             ResponseListener<LatestProcessingResponse> {
             override fun onSuccessfulResponse(response: SuccessfulResponseBody<LatestProcessingResponse>) {
                 val latestProcessing = response.data!![0]
-                Log.d("LATEST_PROCESSING", "response: $latestProcessing")
+                latestProcessingOutcomeListener.onSuccessfulLatestProcessingFetch(
+                    status = latestProcessing.status,
+                    scheduledAt = latestProcessing.scheduledAt,
+                    processedAt = latestProcessing.processedAt,
+                    batchRecords = latestProcessing.batchRecords.map { it.toCoreBatchRecord() },
+                    processedTransactionsCount = latestProcessing.processedTransactionsCount
+                )
             }
 
             override fun onErrorResponse(response: ErrorResponseBody) {
-                Log.d("LATEST_PROCESSING", "error: ${response.message}")
                 latestProcessingOutcomeListener.onFailedLatestProcessingFetch(
                     "An error occurred while fetching latest processing: ${response.message}"
                         ?: "An error occurred while fetching latest processing."
@@ -31,7 +36,6 @@ class LatestProcessingHandler {
             }
 
             override fun onNetworkFailure(t: Throwable) {
-                Log.d("LATEST_PROCESSING", "network error: ${t.message}")
                 latestProcessingOutcomeListener.onFailedLatestProcessingFetch(
                     "Network error: $t.message" ?: "A network error occurred."
                 )
