766c31ee08b0ef57ec9e7284260054db85c27bb9
kmilos-foi <kmilos21@foi.hr>
2025-01-20 23:25:19 +0100

    MI5-105 Ažuriran ProcessingDetailsView
    Dodan je GenerationButtonsRow u view koji pojednostavljuje kod.

--- app/src/main/java/foi/air/szokpt/views/app/ProcessingDetailsView.kt
+++ app/src/main/java/foi/air/szokpt/views/app/ProcessingDetailsView.kt
@@ -3,14 +3,11 @@ package foi.air.szokpt.views.app
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,23 +15,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import foi.air.szokpt.helpers.DateFormatter
-import foi.air.szokpt.ui.components.interactible_components.OutlineBouncingButton
+import foi.air.szokpt.ui.components.clearing_file_generation.GenerationButtonsRow
 import foi.air.szokpt.ui.theme.BGLevelOne
-import foi.air.szokpt.ui.theme.Primary
-import foi.air.szokpt.ui.theme.Secondary
 import foi.air.szokpt.ui.theme.TextWhite
 import foi.air.szokpt.viewmodels.ProcessingDetailsViewModel
 import hr.foi.air.szokpt.core.file_generation.ClearingFileGenerator
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 @Composable
 fun ProcessingDetailsView(
@@ -50,14 +42,6 @@ fun ProcessingDetailsView(
             .padding(16.dp),
         verticalArrangement = Arrangement.Top
     ) {
-        Text(
-            text = "Latest Processing",
-            color = TextWhite,
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 24.dp)
-        )
-
         Surface(
             modifier = Modifier.wrapContentHeight(),
             shape = RoundedCornerShape(16.dp),
@@ -71,21 +55,21 @@ fun ProcessingDetailsView(
                 viewModel.fetchLatestProcessing()
                 when (errorMessage) {
                     null -> {
-                        val latestProcessing = viewModel.latestProcessing.observeAsState().value
-                        if (latestProcessing != null) {
+                        val processingRecord = viewModel.latestProcessing.observeAsState().value
+                        if (processingRecord != null) {
                             Column(
                                 verticalArrangement = Arrangement.spacedBy(4.dp),
                                 modifier = Modifier.padding(start = 5.dp)
                             ) {
                                 Text(
-                                    text = "Status: ${latestProcessing.status}",
+                                    text = "Status: ${processingRecord.status}",
                                     color = TextWhite,
                                     fontSize = 15.sp
                                 )
 
                                 Text(
                                     text = "Scheduled at: ${
-                                        latestProcessing.scheduledAt?.let {
+                                        processingRecord.scheduledAt?.let {
                                             DateFormatter.format(
                                                 it
                                             )
@@ -97,7 +81,7 @@ fun ProcessingDetailsView(
 
                                 Text(
                                     text = "Processed at: ${
-                                        latestProcessing.processedAt?.let {
+                                        processingRecord.processedAt?.let {
                                             DateFormatter.format(
                                                 it
                                             )
@@ -107,42 +91,25 @@ fun ProcessingDetailsView(
                                     fontSize = 15.sp
                                 )
                                 Text(
-                                    text = "Transactions processed: ${latestProcessing.processedTransactionsCount}",
+                                    text = "Transactions processed: ${processingRecord.processedTransactionsCount}",
                                     color = TextWhite,
                                     fontSize = 15.sp
                                 )
                             }
                             Column(
                                 modifier = Modifier.fillMaxWidth(),
-                                verticalArrangement = Arrangement.spacedBy(16.dp)
+                                verticalArrangement = Arrangement.SpaceEvenly
                             ) {
                                 clearingFileGenerators.entries.chunked(2).forEach { chunk ->
-                                    Row(
-                                        modifier = Modifier.fillMaxWidth(),
-                                        horizontalArrangement = Arrangement.SpaceEvenly
-                                    ) {
-                                        chunk.forEach { (label, generator) ->
-                                            OutlineBouncingButton(
-                                                modifier = Modifier.weight(1f),
-                                                inputText = label,
-                                                inputIcon = Icons.AutoMirrored.Filled.ExitToApp,
-                                                contentColor = Primary,
-                                                borderColor = Secondary,
-                                                fontSize = 14.sp,
-                                                spacer = 10.dp
-                                            ) {
-                                                viewModel.viewModelScope.launch {
-                                                    withContext(Dispatchers.IO) {
-                                                        generator.generateFile(
-                                                            processingRecord = latestProcessing,
-                                                            fileName = "processing_report_" +
-                                                                    latestProcessing.processedAt?.let {
-                                                                        DateFormatter.format(it)
-                                                                    }
-                                                        )
-                                                    }
-                                                }
-                                            }
+                                    GenerationButtonsRow(
+                                        chunk,
+                                        processingRecord
+                                    ) { generator, fileName ->
+                                        coroutineScope.launch(Dispatchers.IO) {
+                                            generator.generateFile(
+                                                processingRecord = processingRecord,
+                                                fileName = fileName
+                                            )
                                         }
                                     }
                                 }
