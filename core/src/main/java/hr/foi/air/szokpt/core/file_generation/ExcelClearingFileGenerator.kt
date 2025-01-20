b666782297343bec15c366d176e2b0af657aedd0
NinoLoncar <nino.loncar16@gmail.com>
2025-01-20 20:32:21 +0100

    MI5-106 Dodan kostur za ExcelClearingFileGenerator

---
+++ core/src/main/java/hr/foi/air/szokpt/core/file_generation/ExcelClearingFileGenerator.kt
@@ -0,0 +1,21 @@
+package hr.foi.air.szokpt.core.file_generation
+
+import hr.foi.air.szokpt.core.processing.ProcessingRecord
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+
+class ExcelClearingFileGenerator(private val fileSaver: FileSaver) : ClearingFileGenerator {
+    override suspend fun generateFile(processingRecord: ProcessingRecord, fileName: String) {
+        val workbook = XSSFWorkbook()
+        val sheet = workbook.createSheet("test")
+        val dataRow1 = sheet.createRow(0)
+        dataRow1.createCell(0).setCellValue("test")
+
+        fileSaver.saveFile(
+            "Documents/SZOKPT",
+            "test",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        ) { outputStream ->
+            workbook.write(outputStream)
+        }
+    }
+}
