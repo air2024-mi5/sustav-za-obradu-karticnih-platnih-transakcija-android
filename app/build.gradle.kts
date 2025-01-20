4656e645abd279f782fd7f591fb8a0d48945db83
kmilos-foi <kmilos21@foi.hr>
2025-01-20 21:58:44 +0100

    MI5-105 Postavljeni build.gradleovi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,10 +41,10 @@ android {
 }
 
 dependencies {
-
-    implementation(project(":core"))
     implementation(project(":ws"))
     implementation(project(":excel"))
+    implementation(project(":pdf"))
+    implementation(project(":core"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -62,7 +62,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation("androidx.navigation:navigation-compose:2.8.3")
-
     implementation("com.google.code.gson:gson:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.2.0")
     implementation("com.squareup.retrofit2:retrofit:2.2.0")
