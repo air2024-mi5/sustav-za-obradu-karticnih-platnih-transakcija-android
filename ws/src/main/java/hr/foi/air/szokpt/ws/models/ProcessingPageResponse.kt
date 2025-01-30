d92005c56a6ed96950dee2ef1a2e1d0fb23718a9
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2025-01-31 00:20:48 +0100

    MI5-115 Dodana definicija GET zahtjeva u ProcessingService

--- ws/src/main/java/hr/foi/air/szokpt/ws/models/ProcessingPageResponse.kt
+++ ws/src/main/java/hr/foi/air/szokpt/ws/models/ProcessingPageResponse.kt
@@ -3,7 +3,7 @@ package hr.foi.air.szokpt.ws.models
 import com.google.gson.annotations.SerializedName
 
 data class ProcessingPageResponse(
-    @SerializedName("transactions") val transactions: List<ProcessingResponse>,
+    @SerializedName("transactions") val processingList: List<ProcessingResponse>,
     @SerializedName("currentPage") val currentPage: Int,
     @SerializedName("totalPages") val totalPages: Int
 )
--- ws/src/main/java/hr/foi/air/szokpt/ws/network/ProcessingService.kt
+++ ws/src/main/java/hr/foi/air/szokpt/ws/network/ProcessingService.kt
@@ -2,6 +2,7 @@ package hr.foi.air.szokpt.ws.network
 
 import hr.foi.air.szokpt.core.network.models.SuccessfulResponseBody
 import hr.foi.air.szokpt.core.transactions.SelectedTransactions
+import hr.foi.air.szokpt.ws.models.ProcessingPageResponse
 import hr.foi.air.szokpt.ws.models.ProcessingResponse
 import hr.foi.air.szokpt.ws.models.responses.SelectedTransaction
 import retrofit2.Call
@@ -10,6 +11,7 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ProcessingService {
     @GET("/selected-transactions")
@@ -32,4 +34,10 @@ interface ProcessingService {
     fun revertLastProcessing(
         @Header("Authorization") authHeader: String
     ): Call<SuccessfulResponseBody<Unit>>
+
+    @GET("/processings")
+    fun getAllProcessings(
+        @Query("page") page: Int,
+        @Header("Authorization") authHeader: String
+    ): Call<SuccessfulResponseBody<ProcessingPageResponse>>
 }
