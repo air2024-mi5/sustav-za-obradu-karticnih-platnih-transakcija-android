50af86547cb0a9c0563585e3f191afdb24853f90
phajdarov21 <126573687+phajdarov21@users.noreply.github.com>
2025-01-31 12:54:27 +0100

    MI5-115 Uklonjen fetch zadnje transakcije za ProcessingDetailsView

--- app/src/main/java/foi/air/szokpt/views/app/ProcessingDetailsView.kt
+++ app/src/main/java/foi/air/szokpt/views/app/ProcessingDetailsView.kt
@@ -14,13 +14,10 @@ import androidx.compose.material.icons.rounded.Refresh
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -44,7 +41,6 @@ fun ProcessingDetailsView(
     processingRecord: ProcessingRecord
 ) {
     val viewModel: ProcessingDetailsViewModel = viewModel()
-    val errorMessage by viewModel.errorMessage.observeAsState()
     val coroutineScope = rememberCoroutineScope()
 
     val openRevertProcessingDialog = remember { mutableStateOf(false) }
@@ -65,115 +61,95 @@ fun ProcessingDetailsView(
                 modifier = Modifier.padding(16.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                viewModel.fetchLatestProcessing()
-                when (errorMessage) {
-                    null -> {
-                        if (processingRecord != null) {
-                            Column(
-                                verticalArrangement = Arrangement.spacedBy(4.dp),
-                                modifier = Modifier.padding(start = 5.dp)
-                            ) {
-                                Text(
-                                    text = "Status: ${processingRecord.status}",
-                                    color = TextWhite,
-                                    fontSize = 15.sp
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(4.dp),
+                    modifier = Modifier.padding(start = 5.dp)
+                ) {
+                    Text(
+                        text = "Status: ${processingRecord.status}",
+                        color = TextWhite,
+                        fontSize = 15.sp
+                    )
+                    Text(
+                        text = "Scheduled at: ${
+                            processingRecord.scheduledAt?.let {
+                                DateFormatter.format(
+                                    it
                                 )
-                                Text(
-                                    text = "Scheduled at: ${
-                                        processingRecord.scheduledAt?.let {
-                                            DateFormatter.format(
-                                                it
-                                            )
-                                        }
-                                    }",
-                                    color = TextWhite,
-                                    fontSize = 15.sp
-                                )
+                            }
+                        }",
+                        color = TextWhite,
+                        fontSize = 15.sp
+                    )
 
-                                Text(
-                                    text = "Processed at: ${
-                                        processingRecord.processedAt?.let {
-                                            DateFormatter.format(
-                                                it
-                                            )
-                                        }
-                                    }",
-                                    color = TextWhite,
-                                    fontSize = 15.sp
+                    Text(
+                        text = "Processed at: ${
+                            processingRecord.processedAt?.let {
+                                DateFormatter.format(
+                                    it
                                 )
-                                Text(
-                                    text = "Transactions processed: ${processingRecord.processedTransactionsCount}",
-                                    color = TextWhite,
-                                    fontSize = 15.sp
-                                )
-                                Row(
-                                    modifier = Modifier.fillMaxWidth(),
-                                    horizontalArrangement = Arrangement.Center
-                                ) {
-                                    if (processingRecord.status == "COMPLETED" && revertible) {
-                                        OutlineBouncingButton(
-                                            onClick = {
-                                                openRevertProcessingDialog.value = true
-                                            },
-                                            contentColor = danger,
-                                            borderColor = danger,
-                                            inputIcon = Icons.Rounded.Refresh,
-                                            inputText = "Revert processing",
-                                            modifier = Modifier
-                                        )
-                                    }
-                                }
                             }
-                            Column(
-                                modifier = Modifier.fillMaxWidth(),
-                                verticalArrangement = Arrangement.SpaceEvenly
-                            ) {
-                                clearingFileGenerators.entries.chunked(2).forEach { chunk ->
-                                    GenerationButtonsRow(
-                                        chunk,
-                                        processingRecord
-                                    ) { generator, fileName ->
-                                        coroutineScope.launch(Dispatchers.IO) {
-                                            generator.generateFile(
-                                                processingRecord = processingRecord,
-                                                fileName = fileName
-                                            )
-                                        }
-                                    }
-                                }
-                            }
-                        } else {
-                            Text(
-                                text = "No data about last processing.",
-                                color = TextWhite,
-                                fontSize = 15.sp
+                        }",
+                        color = TextWhite,
+                        fontSize = 15.sp
+                    )
+                    Text(
+                        text = "Transactions processed: ${processingRecord.processedTransactionsCount}",
+                        color = TextWhite,
+                        fontSize = 15.sp
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        if (processingRecord.status == "COMPLETED" && revertible) {
+                            OutlineBouncingButton(
+                                onClick = {
+                                    openRevertProcessingDialog.value = true
+                                },
+                                contentColor = danger,
+                                borderColor = danger,
+                                inputIcon = Icons.Rounded.Refresh,
+                                inputText = "Revert processing",
+                                modifier = Modifier
                             )
                         }
                     }
-
-                    else -> {
-                        Text(
-                            text = errorMessage!!,
-                            color = Color.Red,
-                            fontSize = 15.sp
-                        )
+                }
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    clearingFileGenerators.entries.chunked(2).forEach { chunk ->
+                        GenerationButtonsRow(
+                            chunk,
+                            processingRecord
+                        ) { generator, fileName ->
+                            coroutineScope.launch(Dispatchers.IO) {
+                                generator.generateFile(
+                                    processingRecord = processingRecord,
+                                    fileName = fileName
+                                )
+                            }
+                        }
                     }
                 }
             }
         }
-        if (openRevertProcessingDialog.value) {
-            DialogComponent(
-                dialogTitle = "Revert processing",
-                dialogText = "Are you sure you want to permanently revert transaction processing?\n",
-                highlightColor = danger,
-                titleColor = TextWhite,
-                iconTop = Icons.Rounded.Refresh,
-                onConfirmation = {
-                    viewModel.revertLastProcessing()
-                    openRevertProcessingDialog.value = false
-                },
-                onDismissRequest = { openRevertProcessingDialog.value = false },
-            )
-        }
+
+    }
+    if (openRevertProcessingDialog.value) {
+        DialogComponent(
+            dialogTitle = "Revert processing",
+            dialogText = "Are you sure you want to permanently revert transaction processing?\n",
+            highlightColor = danger,
+            titleColor = TextWhite,
+            iconTop = Icons.Rounded.Refresh,
+            onConfirmation = {
+                viewModel.revertLastProcessing()
+                openRevertProcessingDialog.value = false
+            },
+            onDismissRequest = { openRevertProcessingDialog.value = false },
+        )
     }
 }
