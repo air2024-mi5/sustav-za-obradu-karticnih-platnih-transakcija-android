290df9519d9c4fea5f5f83369c4c6cf356a2ba61
kmilos-foi <kmilos21@foi.hr>
2025-02-01 00:05:43 +0100

    Refaktoriran DashboardView
    Obrisani su elementi koji se ne koriste i koji su hardkodirani.

--- app/src/main/java/foi/air/szokpt/views/app/DashboardView.kt
+++ app/src/main/java/foi/air/szokpt/views/app/DashboardView.kt
@@ -5,12 +5,9 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -36,10 +33,7 @@ import foi.air.szokpt.ui.components.dashboard_components.TransactionsPerDay
 import foi.air.szokpt.ui.theme.BGLevelOne
 import foi.air.szokpt.ui.theme.TextWhite
 import foi.air.szokpt.ui.theme.TileSizeMode
-import foi.air.szokpt.ui.theme.danger
-import foi.air.szokpt.ui.theme.success
 import foi.air.szokpt.utils.CardBrandInformationList
-import foi.air.szokpt.utils.TransactionUtils
 import foi.air.szokpt.viewmodels.CardBrandsStatisticsViewModel
 import foi.air.szokpt.viewmodels.ReportsViewModel
 import foi.air.szokpt.viewmodels.TransactionsPerDayViewModel
@@ -60,13 +54,10 @@ fun DashboardView() {
         horizontalArrangement = Arrangement.spacedBy(4.dp)
     ) {
         item(span = { GridItemSpan(2) }) {
-            ValueTile()
-        }
-        item(span = { GridItemSpan(2) }) {
             TransactionsPerDayTile()
         }
         item(span = { GridItemSpan(2) }) {
-            CardBrandsTile(transactionUtils = TransactionUtils)
+            CardBrandsTile()
         }
         item(span = { GridItemSpan(2) }) {
             TransactionOutcomes()
@@ -75,89 +66,7 @@ fun DashboardView() {
 }
 
 @Composable
-fun ValueTile() {
-    TileSegment(
-        tileSizeMode = TileSizeMode.FILL_MAX_SIZE,
-        innerPadding = 4.dp,
-        outerMargin = 8.dp,
-        minWidth = 150.dp,
-        minHeight = 200.dp,
-        color = BGLevelOne
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(8.dp)
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.Start,
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = "Value",
-                color = Color.White,
-                fontSize = 22.sp,
-                fontWeight = FontWeight.Bold
-            )
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 4.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = "€1 002 123",
-                    color = success,
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
-                )
-            }
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 4.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Start
-            ) {
-                Text(
-                    text = "This week:",
-                    color = Color.White,
-                    fontSize = 14.sp
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(
-                    text = "-9.8%",
-                    color = danger,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 4.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = "+ €26 017",
-                    color = success,
-                    fontSize = 22.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
-        }
-    }
-}
-
-@Composable
 fun CardBrandsTile(
-    transactionUtils: TransactionUtils,
     viewModel: CardBrandsStatisticsViewModel = viewModel()
 ) {
     val cardBrandsStats by viewModel.cardBrandsStatisticsData.observeAsState()
@@ -239,15 +148,6 @@ fun TransactionsPerDayTile(
     viewModel.fetchTransactionsPerDay()
     when (errorMessage) {
         null -> {
-            val sampleDayData = listOf(
-                TransactionsPerDay(LocalDate.of(2024, 1, 13), 14),
-                TransactionsPerDay(LocalDate.of(2024, 1, 14), 28),
-                TransactionsPerDay(LocalDate.of(2024, 1, 15), 8),
-                TransactionsPerDay(LocalDate.of(2024, 1, 16), 14),
-                TransactionsPerDay(LocalDate.of(2024, 1, 17), 28),
-                TransactionsPerDay(LocalDate.of(2024, 1, 18), 8),
-                TransactionsPerDay(LocalDate.of(2024, 1, 19), 8),
-            )
             val transactionData = transactionsPerDay?.map { (timestamp, count) ->
                 TransactionsPerDay(
                     date = LocalDate.of(
